374ee68eb9406942d12b7d94d3738204ef2f49b5
Sanjay Singh <contact.sanjaysingh23@gmail.com>
2026-03-01 22:02:34 +0530

    Time: 0 ms (100%), Space: 43.4 MB (34.16%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+
+        int count = 0 ; 
+
+        for(int i=0;i<stones.length();i++){
+            char ch = stones.charAt(i);
+            for(int j=0;j<jewels.length();j++){
+                if(jewels.charAt(j)==ch)
+                  count++;
+            }
+        }
+        
+        return count;
+    }
+}
