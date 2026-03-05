42976130ac541f2cd1ed66de0f3e38fb1af44638
Sanjay Singh <contact.sanjaysingh23@gmail.com>
2026-03-05 20:15:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,16 +1,12 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
 
-        StringBuilder sb = new StringBuilder();
         int N = indices.length;
+        char arr[] = new char[N];
 
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(indices[j]==i)
-                  sb.append(s.charAt(j));
-            }
+        for(int i=0;i<N;i++)
+           arr[indices[i]] = s.charAt(i); 
+
+          return String.valueOf(arr); 
     }
-
-    return sb.toString();
-  }
 }
