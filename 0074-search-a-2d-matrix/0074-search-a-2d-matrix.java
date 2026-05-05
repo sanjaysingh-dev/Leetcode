2e000ace531cf6ce263e78afd9400a0ff9e2aa0f
Sanjay Singh <contact.sanjaysingh23@gmail.com>
2026-05-05 16:26:59 +0530

    Time: 0 ms (100%), Space: 43.8 MB (61.13%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -4,21 +4,24 @@ class Solution {
         int N = matrix.length;
         int M = matrix[0].length;
 
-        int i = 0;
-        int j = M-1;
+        int low = 0, high =(N*M)-1;
 
-        while(i<N && j>=0){
+        while(low<=high){
 
-                if(matrix[i][j] == target)
-                  return true;
+            int mid = low +(high-low)/2;
+            int i = mid/M;
+            int j = mid%M;
 
-                else if(target>matrix[i][j])
-                  i++;
+            if(matrix[i][j] == target)
+             return true;
 
-                else 
-                 j--;    
-            }
-        
+            else if(matrix[i][j]>target)
+             high = mid - 1;
+
+            else 
+             low = mid + 1; 
+        }
+
         return false;
     }
 }
