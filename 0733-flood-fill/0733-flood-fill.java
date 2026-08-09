5c6712a80831388083512087327c83951f2cd7e4
Sanjay Singh <contact.sanjaysingh23@gmail.com>
2026-08-09 20:24:21 +0530

    Time: 1 ms (38.42%), Space: 46.9 MB (44.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,34 +4,26 @@ class pair{
     int col;
 
     public pair(int row,int col){
-        this.row = row;
-        this.col = col;
+        this.row=row;
+        this.col=col;
     }
 }
 
 class Solution {
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
         int N = image.length;
         int M = image[0].length;
 
-        int ocolor = image[sr][sc];
+        int origC = image[sr][sc];
 
-        if(ocolor == color)
-          return image;
-  
-        int vis[][] = new int[N][M];
+        if(origC==color)
+         return image;
 
-        for(int i =0;i<N;i++)
-            vis[i] = image[i].clone();
-
-         vis[sr][sc] = color;   
-
+        image[sr][sc] = color;
 
         Queue<pair> q = new LinkedList<>();
-
-        q.offer(new pair(sr,sc));
+        q.offer(new pair(sr,sc)); 
 
         int dr[] = {-1,1,0,0};
         int dc[] = {0,0,-1,1};
@@ -43,20 +35,19 @@ class Solution {
             int col = p.col;
 
             for(int i=0;i<4;i++){
-                int newrow = row + dr[i];
-                int newcol = col + dc[i];
+                int newr = row+dr[i];
+                int newc = col+dc[i];
 
-                if(newrow>=0 && newrow<N && newcol>=0 && newcol<M &&
-                    vis[newrow][newcol]==ocolor){
+                if(newr>=0 && newr<N && newc>=0 && newc<M && 
+                   image[newr][newc]== origC){
 
-                        vis[newrow][newcol] = color;
-                        q.offer(new pair(newrow,newcol));
-                    }
+                    image[newr][newc] = color;
+                    q.offer(new pair(newr,newc));
+
+                   }
             }
-
         }
-
-        return vis;
         
+        return image;
     }
 }
